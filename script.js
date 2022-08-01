9cb1dca9e69d03f7006fe4550530d0b8e21c4138
maverickJAI <jaimech61@gmail.com>
2022-08-01 14:10:33 +0530

    completed till local stroge

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ const addButton = document.getElementById("cross-btn");
 const numberOfTaskleft = document.getElementById("number-of-task");
 const textBar = document.getElementById("text-bar"); // TODO INPUT
 const todoList = document.getElementsByClassName("todo-list")[0];
-const filterSelector = document.querySelector("task-number-right");
+const filterSelector = document.getElementsByClassName("task-number-right")[0];
 const numberOfTaskRemaining = document.querySelector("task-number-left");
 
+
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getTodoFromLocal);
 addButton.addEventListener("click", addTodo);
 todoList.addEventListener('click', deleteCheck);
 todoList.addEventListener('click', completedCheck);
+filterSelector.addEventListener('click', filterTodo);
 
 
 
@@ -33,6 +36,7 @@ function addTodo(event) {
     newTodo.innerHTML = textBar.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+    saveLocalTodo(textBar.value);
 
     // CHECK MARK BUTTON
     const completedButtom = document.createElement("button");
@@ -45,9 +49,12 @@ function addTodo(event) {
     trashButtom.innerHTML = 'DEL';
     trashButtom.classList.add("trash-btn");
     todoDiv.appendChild(trashButtom)
+    
 
     // APPENDT TO LIST
     todoList.appendChild(todoDiv);
+    // SAVING TO LOCAL STORAGE
+    
 
     // CLEAR TODO INPUT VALUE
     textBar.value = "";
@@ -61,7 +68,6 @@ function addTodo(event) {
 
 
 // FUNCTION FOR DELETE CHECK
-
 function deleteCheck(event) {
     // console.log(event.target);
     const item = event.target;
@@ -73,11 +79,10 @@ function deleteCheck(event) {
             trashButtonParent.remove();
         });
     }
-    todoitems = todoitems - 1;
+    deleteLocalTodo();
 }
 
 // FUNCTION FOR COMPLETED  CHECK
-
 function completedCheck(event) {
     console.log(event.target);
     const item = event.target;
@@ -89,9 +94,122 @@ function completedCheck(event) {
     todoitems = todoitems - 1;
 }
 
-numberOfTask();
-function numberOfTask(params) {
-    const length = document.getElementsByClassName("todo").childElementCount;
-    numberOfTaskRemaining.innertext = `${length} task left`;
+// DUNCTION FOR FILTERING ALL AND COMPLETED
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        const mstyle = todo.style
+        if (mstyle != undefined && mstyle != null) {
+            switch (event.target.value) {
+                case "all":
+                    todo.style.display = "flex";
+                    break
+    
+                case "completed":
+                    if(todo.classList.contains("completed")){
+                        todo.style.display = "flex";
+                    }else{
+                         todo.style.display = "none"
+                    }
+                    break;
+    
+                case "in-complete":
+                        if(todo.classList.contains("completed")){
+                            todo.style.display = "none";
+                        }else{
+                             todo.style.display = "display"
+                        }
+                        break;
+                default:
+                    break;
+            }
+        }
+            
+        })
+        
+    }
+
+// FUNCTION FOR SAVING LOCAL
+function saveLocalTodo(todo) {
+    // check if its already saved.
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos= [];
+
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+    
 }
 
+// FUNCTIIN FOR GETTING THE VALUES FORM LOCALSTORAGE
+function getTodoFromLocal() {
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todoS = [];
+
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+
+    todos.forEach(function(todo) {
+          // Todo DIV
+     const todoDiv = document.createElement("div");
+     todoDiv.classList.add("todo");
+ 
+     // create li
+     const newTodo = document.createElement("li");
+     newTodo.innerHTML = todo;
+     newTodo.classList.add("todo-item");
+     todoDiv.appendChild(newTodo);
+ 
+     // CHECK MARK BUTTON
+     const completedButtom = document.createElement("button");
+     completedButtom.innerHTML = 'DONE';
+     completedButtom.classList.add("complete-btn");
+     todoDiv.appendChild(completedButtom);
+ 
+     // CHECK TRASH BUTTON
+     const trashButtom = document.createElement("button");
+     trashButtom.innerHTML = 'DEL';
+     trashButtom.classList.add("trash-btn");
+     todoDiv.appendChild(trashButtom)
+ 
+     // APPENDT TO LIST
+     todoList.appendChild(todoDiv);
+ 
+
+    })
+}
+  // FUCTION TO DELETE THE ONJECTS IN LOCAL STORAGE
+function deleteLocalTodo() {
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todoS = [];
+
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+
+    const todoIndex = todos.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+
+
+
+
+
+
+
+
+
+// numberOfTask();
+// function numberOfTask(params) {
+//     const length = document.getElementsByClassName("todo").childElementCount;
+//     numberOfTaskRemaining.innertext = `${length} task left`;
+// }
